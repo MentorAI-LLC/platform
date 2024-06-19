9c406daf3935dd435f27411ca564214ca370fdd5
0x61nas <anas.elgarhy.dev@gmail.com>
2024-06-20 00:16:08 +0300

    chore(git): setup pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -10,26 +10,34 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
       pkgs = nixpkgs.legacyPackages.${system};
       pname = ""; # <same as package.json name>
       version = "0.1.0";
       buildInputs = with pkgs; [
         nodejs_20
         nodePackages_latest.pnpm
+        pre-commit
       ];
       nativeBuildInputs = buildInputs;
+      devPkgs = with pkgs; [ pre-commit ];
       npmDepsHash = ""; # <prefetch-npm-deps package-lock.json>
-    in {
+    in
+    {
       devShells.default = pkgs.mkShell {
         inherit buildInputs;
+        packages = devPkgs;
         shellHook = ''
           #!/usr/bin/env bash
+          if ! grep -q "pre-commit" .git/hooks/pre-commit 2>/dev/null; then
+              pre-commit install
+          fi
         '';
       };
       packages.default = pkgs.buildNpmPackage {
