7f4d360a37e9b4c04327fd39488d4bc917e37e07
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2024-06-28 13:59:27 +0300

    (feat)finishing up start section

--- src/Sections/StartSection.tsx
+++ src/Sections/StartSection.tsx
@@ -1,6 +1,13 @@
 import Button from '@/components/Button';
 import React from 'react';
 
+/**
+ * StartSection Component
+ *
+ * A functional component that represents the start section of the webpage. It includes a heading and a call-to-action button, prompting users to sign up.
+ *
+ * @returns {JSX.Element} A JSX element representing the start section.
+ */
 function StartSection() {
   return (
     <section className="bg-[#9BD6E7] mt-80 w-full h-[350px] flex flex-col items-center justify-center gap-6 md:gap-10">
